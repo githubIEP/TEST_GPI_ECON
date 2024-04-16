8659e05164b519ccbfd8495f0ab232cdf7f265f3
briahnatandoh-iep <btandoh@economicsandpeace.org>
2024-04-16 11:50:50 +1000

    more tables

--- 03_scripts/02_standardOutputs/02_tables.R
+++ 03_scripts/02_standardOutputs/02_tables.R
@@ -15,9 +15,22 @@ library(scales)
 wb_Tables <- createWorkbook()
 
 ### --- List of Standard Tables
-# Military expenditure: total, per capita, and as percentage of GDP
 
-TABLE_MEx = c(title = "Military expenditure: total, per capita, and as percentage of GDP",
+# Change in the global economic impact of violence, billions of PPP 2022 US dollars, 2021–2022
+
+TABLE_ImpactChange = c(title = "Change in the global economic impact of violence, billions of PPP 2022 US dollars, 2021–2022",
+                            sheet = "ImpactChange", source = "IEP Calculations", xtext = "", ytext = "",
+                            type = "Table", position = "Normal")
+
+# Change in the global economic impact of violence, billions of PPP 2022 US dollars, 2008–2022
+
+TABLE_ImpactChangeTrend = c(title = "Change in the global economic impact of violence, billions of PPP 2022 US dollars, 2008–2022",
+              sheet = "ImpactChangeTrend", source = "IEP Calculations", xtext = "", ytext = "",
+              type = "Table", position = "Normal")
+
+# Military expenditure: total, per capita, and as percentage of GDP, 2022
+
+TABLE_MEx = c(title = "Military expenditure: total, per capita, and as percentage of GDP, 2022",
                        sheet = "MEx", source = "IEP Calculations", xtext = "", ytext = "",
                        type = "Table", position = "Normal")
 
@@ -30,6 +43,53 @@ TABLE_TenCountries = c(title = "The Ten Countries with the Highest Economic Cost
 ### --- Loading Data
 
 econ_impact.df <- rio::import("04_outputs/Economic Impact of Violence.xlsx")
+
+## -- TABLE_ImpactChange -----------------------------------------------------------
+# A table showing the change in the global 
+# economic impact of violence, billions of PPP 2022 US dollars, 2021–2022
+
+tab_Change <- econ_impact.df %>%
+  dplyr::filter(year==max(year), subtype=="costusd") %>%
+  pivot_wider(names_from = type, values_from = value) %>%
+  replace(is.na(.), 0) %>%
+  group_by(indicator2, year) %>%
+  summarise(direct = sum(direct)/1000000000, direct = round(direct, 0),
+            indirect = sum(indirect)/1000000000, indirect = round(indirect, 0)) %>%
+  mutate(multiplier = direct, total_impact = sum(direct, indirect, multiplier)) %>%
+  select(-year)
+
+
+tab_Change_2021 <- econ_impact.df %>%
+  dplyr::filter(year==max(year-1), subtype=="costusd") %>%
+  pivot_wider(names_from = type, values_from = value) %>%
+  replace(is.na(.), 0) %>%
+  group_by(indicator2, year) %>%
+  summarise(direct = sum(direct)/1000000000, direct = round(direct, 0),
+            indirect = sum(indirect)/1000000000, indirect = round(indirect, 0)) %>%
+  mutate(multiplier = direct, total_impact_2021 = sum(direct, indirect, multiplier)) %>%
+  select(indicator2, total_impact_2021)
+
+tab_Change <- tab_Change %>%
+  left_join(tab_Change_2021, by = "indicator2") %>%
+  mutate(total_change = total_impact-total_impact_2021, `PERCENTAGE CHANGE` = total_change/total_impact_2021*100, 
+         `PERCENTAGE CHANGE` = round(`PERCENTAGE CHANGE`, 1)) %>%
+  arrange(desc(total_impact)) %>%
+  rename(Indicator = indicator2)
+
+## -- TABLE_ImpactChangeTrend -----------------------------------------------------------
+# A table showing the change in the global 
+# economic impact of violence, billions of PPP 2022 US dollars, 2008–2022
+
+tab_ChangeTrend <- econ_impact.df %>%
+  dplyr::filter(year %in% c (min(year), max(year)), subtype=="impact") %>%
+  group_by(indicator2, year) %>%
+  summarise(value = sum(value)/1000000000, value = round(value, 1)) %>%
+  pivot_wider(names_from = year, values_from = value) %>%
+  mutate(BILLIONS = `2022`-`2008`, `PERCENTAGE CHANGE` = BILLIONS/`2008`*100, 
+         `PERCENTAGE CHANGE` = round(`PERCENTAGE CHANGE`, 0)) %>%
+  arrange(desc(`PERCENTAGE CHANGE`)) %>%
+  rename(Indicator = indicator2)
+
 
 ## -- TABLE_MEx -----------------------------------------------------------
 # Three tables showing the ten countries with the highest total military expenditure,
